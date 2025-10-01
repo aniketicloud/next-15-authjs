138618e57f348227b752f3cb93443f10e70c4fc5
Aniket Pandharabale <aniketicloud@gmail.com>
2025-10-02 00:12:20 +0530

    feat: add login form component and integrate theme switcher in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeSwitcher } from "@/components/ui/shadcn-io/theme-switcher";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <ThemeSwitcher />
           {children}
         </ThemeProvider>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import { ThemeSwitcher } from "@/components/ui/shadcn-io/theme-switcher";
-
 export default function Home() {
   return (
     <>
-      <ThemeSwitcher />
       <h1>Hello</h1>
     </>
   );
